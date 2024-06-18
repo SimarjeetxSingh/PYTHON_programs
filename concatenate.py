993a1db05e636e35c62a094cfa6faca826e8a732
SIMARJEET SINGH <130891817+SimarjeetxSingh@users.noreply.github.com>
2024-06-19 04:14:51 +0530

    Create concatenate.py

---
+++ concatenate.py
@@ -0,0 +1,13 @@
+"""Concatenate
+Yeah, big word.
+All it really means is combine text (remember, text is called a string) and variables together into single sentences! 😲🤯
+
+You can make your input and output look super pretty now! 🥳"""
+food=input("Which food are you cooking: ")
+print("Which method are you using for cooking",food)
+method=input(":")
+plant=input("Name a plant you will add in it : ")
+burned=input("What word describes burned food: ")
+item=input("Name a household item: ")
+print("****Special Concatenated Recipe**** ")
+print(method,food,"with", burned, plant,"on a bed of",item)
