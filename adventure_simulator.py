a3cb76db12ef8d9f21b74b759e6e022bf37e7920
SIMARJEET SINGH <130891817+SimarjeetxSingh@users.noreply.github.com>
2024-06-19 23:18:04 +0530

    Create adventure_simulator.py

---
+++ adventure_simulator.py
@@ -0,0 +1,21 @@
+print(""" ====== ADVENTURE SIMULATOR ======
+Welcome to Your adventure Simulator. I am goint to ask you
+a bunch of questions and then create an epic story with you as
+the star!🤩
+""")
+print()
+name=input(" What is Your Name: ")
+superpower=input(" What is Your SuperPower: ")
+enemy=input(" Your worst enemy's name: ")
+print()
+print()
+print("--------------------------------------------------------")
+print("Our Story begins ...")
+print(" Our Hero \033[31m",name,"\033[0m"" approaches a foreboding castle...")
+print("Suddenly a bolt of lightning striked the ground at the feet of\033[31m ", name)
+print(" \033[0m","'Our final battle begins! ' shouts the evil","\033[33m",enemy)
+print("\033[0mclearly missing the fact that ")
+print("\033[31m",name, "\033[0m has the power of \033[36m",superpower)
+print("\033[0m which means they'll win quite easily")
+print("After fighting a fearsome battle our hero \033[31m wins \033[0m ⚔️")
+print("--------------------------------------------------------")
